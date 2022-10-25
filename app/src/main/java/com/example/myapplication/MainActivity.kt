c12151f8b053ef05c9b80b637d78a4c6b11baa47
itssinghnakit <singhankit.kr@com.gmail>
2022-10-25 22:05:54 +0530

    basic layout

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Spinner
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Spinner spn= findViewById<Spinner>(R.id.spinner)
+
+
+    }
+}
